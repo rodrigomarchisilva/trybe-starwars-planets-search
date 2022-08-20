2e71044c40a21b15bd5279642bdfad39aae3e53a
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2022-08-20 20:12:54 -0300

    :lipstick: style: add placeholders on loading component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import { Placeholder, Row, Col } from 'react-bootstrap';
 
-const Loading = () => <div>Loading...</div>;
+export default function Loading() {
+  const ROWS = 13;
+  const COLS = 3;
 
-export default Loading;
+  Col.defaultProps = { xs: 4 };
+
+  return (
+    <div>
+      {
+        Array.from({ length: ROWS }).map((_, i) => (
+          <Row className="ms-1 mb-2 me-1" key={ i }>
+            {
+              Array.from({ length: COLS }).map((_2, j) => (
+                <Placeholder as={ Col } className="ps-1 pe-1" key={ j } animation="glow">
+                  <Placeholder size="lg" className="rounded-1 p-3 w-100" />
+                </Placeholder>
+              ))
+            }
+          </Row>
+        ))
+      }
+    </div>
+  );
+}
